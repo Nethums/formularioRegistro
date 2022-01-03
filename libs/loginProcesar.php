939cb6b549082b1e7cec630545c65c115ca762a3
Miguel Angel G <magallart58@gmail.com>
2022-01-03 13:24:35 +0100

    galeria.php modificada
    Ya se muestran las imágenes en la carpeta del usuario.

--- libs/loginProcesar.php
+++ libs/loginProcesar.php
@@ -2,17 +2,13 @@
 <?php
     include ('bConecta.php');
 
-    /*
-    * Es la manera de evitar la inyección SQL
-    */
-
     $usuario = $_REQUEST['usuarioLogin'];
     $clave = $_REQUEST['contrasenaLogin'];
 
     $consulta = "SELECT * FROM usuarios 
         WHERE user=:usuario
         AND pass=:clave";
-    $result = $db->prepare($consulta);
+    $result = $pdo->prepare($consulta);
     $result -> execute(array(":usuario" => $usuario, ":clave" => $clave));
     $filasConsulta = $result -> rowCount();  //Contamos las filas devueltas por la consulta $result
 
@@ -20,7 +16,7 @@
         //Añadimos un mensaje por GET para recogerlo en la página index.php y mostrar un mensaje de error en caso de que la identificación haya sido
         header('Location: ../index.php?noLogin=error');
     } else {
-        header('Location: ../pages/galeria.php');
+        header('Location: ../pages/galeria.php?usuario=' . $usuario);
     }
 
 ?>
